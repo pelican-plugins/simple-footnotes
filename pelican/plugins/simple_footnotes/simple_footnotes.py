9de551674aeb1dcb7396535cbce9c76b790c5dbd
Sogolumbo <33571916+Sogolumbo@users.noreply.github.com>
2021-08-26 04:49:33 +0200

    Fix #7 (Footnotes not compiled on hidden pages)
    https://github.com/pelican-plugins/simple-footnotes/issues/7

--- pelican/plugins/simple_footnotes/simple_footnotes.py
+++ pelican/plugins/simple_footnotes/simple_footnotes.py
@@ -29,7 +29,8 @@ def sequence_gen(genlist):
 def parse_for_footnotes(article_or_page_generator):
     all_content = [
         getattr(article_or_page_generator, attr, None)
-        for attr in ["articles", "drafts", "pages"]
+        for attr in ["articles", "translations", "drafts", "drafts_translations",
+                     "pages", "translations", "hidden_pages", "hidden_translations", "draft_pages", "draft_translation"]
     ]
     all_content = [x for x in all_content if x is not None]
     for article in sequence_gen(all_content):
